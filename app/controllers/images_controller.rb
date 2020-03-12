7b55db2b1ffb572629085776b8e1e35902fe9204
GERMAN GARZON <germangarzon@GERMANs-MacBook-Air.local>
2020-03-12 14:30:27 -0500

    random

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,8 +1,8 @@
 class ImagesController < ApplicationController
   def new
-      #response = HTTParty.get 'https://apodapi.herokuapp.com/api/?start_date=2018-10-05&end_date=2018-10-10&thumbs=true&image_thumbnail_size=480&image_thumbnail_size=240'
-      response = HTTParty.get("https://apodapi.herokuapp.com/api")
-      render json: JSON.parse(response.body)
+      response = HTTParty.get 'https://apodapi.herokuapp.com/api/?start_date=2018-10-05&end_date=2018-10-10&thumbs=true&image_thumbnail_size=480&image_thumbnail_size=240'
+      # response = HTTParty.get("https://apodapi.herokuapp.com/api")
+      render json: JSON.parse(response.body).sample
 
   end
 end
